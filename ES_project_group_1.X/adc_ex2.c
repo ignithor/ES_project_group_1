132cfe39ab13ddea746f82b189f56ea562c86635
Mamo1031 <mamoru_ota1031@keio.jp>
2025-06-12 16:46:08 +0900

    add: files about adc

---
+++ ES_project_group_1.X/adc_ex2.c
@@ -0,0 +1,13 @@
+/*
+ * File:   adc_ex2.c
+ * Author: Mamoru
+ *
+ * Created on 2025/06/12, 16:42
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
---
+++ ES_project_group_1.X/adc_ex3.c
@@ -0,0 +1,13 @@
+/*
+ * File:   adc_ex3.c
+ * Author: Mamoru
+ *
+ * Created on 2025/06/12, 16:43
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
