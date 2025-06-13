f0725108da0ee62230981c84556f00e20d45c2bf
Paul P <paul.phamdang@gmail.com>
2025-06-13 14:59:22 +0200

    side led working

--- ES_project_group_1.X/main.c
+++ ES_project_group_1.X/main.c
@@ -15,17 +15,26 @@
 
 // LED alias definitions for clarity in code.
 #define LED1 LATAbits.LATA0
+// Define TURN signal pins
+#define TURN_L LATFbits.LATF1
+#define TURN_R LATBbits.LATB8
 
 int main(void) {
     // Initialize pins
     TRISAbits.TRISA0 = 0; // pin A0 set as output (LED1)
-
+    TRISFbits.TRISF1=0;
+    TRISBbits.TRISB8=0;
+    
     // Disable all analog functionality on pins to use them as digital I/O
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
 
     LED1 = 1; // LED initially on
     int tmr_counter_led = 0;
 
+        // Initialize state
+    TURN_L = 0;
+    TURN_R = 0;
+    
     // Configure timers
     tmr_setup_period(TIMER1, 2); // TIMER1: 500Hz main loop timing (2ms)
 
@@ -33,8 +42,10 @@ int main(void) {
     while (1) {
 
         // If 1000ms waited switch the LED
-        if (tmr_counter_led == 1000) {
+        if (tmr_counter_led == 500) {
             LED1 = !LED1;
+            TURN_L = !TURN_L;
+            TURN_R = !TURN_R;
             tmr_counter_led = 0;
         }
 
