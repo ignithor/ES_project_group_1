62448eae2c00045f1de8d8051c9b85d0f59756cf
Mamo1031 <mamoru_ota1031@keio.jp>
2025-06-13 14:08:22 +0900

    feat: Enable motor control with button press

--- ES_project_group_1.X/main.c
+++ ES_project_group_1.X/main.c
@@ -12,9 +12,39 @@
 #include "timer.h"
 #include "uart.h"
 
+int is_pwm_on; // Flag - 1: PWM is generated 0: NO
 
 // LED alias definitions for clarity in code.
 #define LED1 LATAbits.LATA0
+
+// Interrupt routine for pressed button
+void __attribute__((__interrupt__, __auto_psv__))_INT1Interrupt(void) {
+   IFS1bits.INT1IF = 0; // clear the interrupt flag
+   IEC1bits.INT1IE = 0; // disable interrupt
+
+   is_pwm_on = !is_pwm_on;
+
+   if (is_pwm_on) {
+       LATAbits.LATA0 = 1;
+       drive_forward();
+
+   } else {
+       LATAbits.LATA0 = 0;
+       drive_stop();
+   }
+
+   tmr_setup_period(TIMER2, 20);
+    IEC0bits.T2IE = 1;
+}
+
+// Debouncing using two interrupts
+void __attribute__((__interrupt__, __auto_psv__)) _T2Interrupt(void){
+   IFS0bits.T2IF = 0;      // clear the flag on the interrupt 2
+   IEC0bits.T2IE = 0;      // disable the interrupt of the timer 2
+   
+   IEC1bits.INT1IE = 1;
+//    LATAbits.LATA0 = !LATAbits.LATA0;   
+}
 
 int main(void) {
     // Initialize pins
@@ -26,8 +56,23 @@ int main(void) {
     LED1 = 1; // LED initially on
     int tmr_counter_led = 0;
 
+    is_pwm_on = 0; // Flag - 1: PWM ON, 0: OFF
+
+    // Set pin as input
+   TRISEbits.TRISE8 = 1; // T2 button set as input
+   TRISEbits.TRISE9 = 1; // T3 button set as input
+
+   /*Enable interrupts in INT1*/
+   RPINR0bits.INT1R = 0x58;    //RPI88 in hex (88)
+   INTCON2bits.GIE = 1;        // set global interrupt enable
+   IFS1bits.INT1IF = 0;        // clear the interrupt flag
+   IEC1bits.INT1IE = 1;        // enable interrupt
+
+   init_pwm();
+
     // Configure timers
     tmr_setup_period(TIMER1, 2); // TIMER1: 500Hz main loop timing (2ms)
+    tmr_setup_period(TIMER2, 20);
 
 
     while (1) {
