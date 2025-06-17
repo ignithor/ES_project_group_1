a079645cd1fda0b6084567041c3a0708363a1409
Yui <yuimomi0513@gmail.com>
2025-06-17 16:05:11 +0200

    Clean-up useless lines

--- ES_project_group_1.X/main.c
+++ ES_project_group_1.X/main.c
@@ -5,7 +5,6 @@
  * Created on June 9, 2025, 5:32 PM
  */
 
-
 #include "xc.h"
 #include "pwm.h"
 #include "spi.h"
@@ -16,14 +15,6 @@
 #include <string.h>
 #include <math.h>
 
-
-// LED alias definitions for clarity in code.
-#define LED1 LATAbits.LATA0
-
-extern volatile char rxBuffer[RX_BUFFER_SIZE];
-extern volatile uint16_t rxIndex;
-extern volatile uint8_t rxStringReady;
-extern uint8_t currentRate; // Initialize with 5 Hz as required
 extern uint8_t currentRate; // Initialize with 5 Hz as required
 extern int x_values_acc[ARRAY_SIZE];
 extern int y_values_acc[ARRAY_SIZE];
@@ -34,14 +25,8 @@ unsigned int uart_period_ms = 200;
 char localCopy[RX_BUFFER_SIZE];
 
 int main(void) {
-    // Initialize pins
-    TRISAbits.TRISA0 = 0; // pin A0 set as output (LED1)
-
     // Disable all analog functionality on pins to use them as digital I/O
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
-
-    LED1 = 1; // LED initially on
-    int tmr_counter_led = 0;
 
     // Configure timers
     tmr_setup_period(TIMER1, 2); // TIMER1: 500Hz main loop timing (2ms)
@@ -55,49 +40,23 @@ int main(void) {
     // Configure accelerometer
     accelerometer_config();
 
-    // Configure timers
-    tmr_setup_period(TIMER1, 2); // TIMER1: 500Hz main loop timing (10ms)
-
-
     // Tracks elapsed time in main loop
     int tmr_counter_accelerometer = 0;
     int tmr_counter_uart = 0;
 
     char acc_message[32]; // Buffer for ACC message
 
-    // Configure timers
-    tmr_setup_period(TIMER1, 2); // TIMER1: 500Hz main loop timing (2ms)
-
+    // Set the average of accelerometer offset when "wait for start" state
+    int x_bias = -70;  
+    int y_bias = -94;
+    int z_bias = 983;
 
     while (1) {
-
-        // if (rxStringReady) {
-        //     strcpy(localCopy, (char*) rxBuffer); // Copy safely
-        //     rxStringReady = 0; // Reset flag AFTER copy
-        //     rxIndex = 0; // Also reset index here (not inside ISR)
-
-        //     UART_SendString("You entered: ");
-        //     UART_SendString(localCopy);
-        //     UART_SendString("\r\n");
-        //     process_uart_command(localCopy);
-        // }
-
-
-        // If 1000ms waited switch the LED
-        if (tmr_counter_led == 1000) {
-            LED1 = !LED1;
-            tmr_counter_led = 0;
-        }
-
-        // // Acquire accelerometer data at 10Hz (every 100ms)
+        // Acquire accelerometer data at 10Hz (every 100ms)
         if (++tmr_counter_accelerometer >= 100) {
             acquire_accelerometer_data();
             tmr_counter_accelerometer = 0;
         }
-        
-        int x_bias = -70;  // average when stopping
-        int y_bias = -94;
-        int z_bias = 983;
 
         // Calculate averages of last 5 measurements once per main loop iteration
         int x_acc = filter_acc(x_values_acc, ARRAY_SIZE)-x_bias;
@@ -116,7 +75,6 @@ int main(void) {
 
         // Maintain precise 500Hz loop timing
         tmr_wait_period(TIMER1); // Wait for timer period completion
-        tmr_counter_led += 2; // Increment counters by 2ms
         tmr_counter_accelerometer += 10;
         tmr_counter_uart += 10;
     }
